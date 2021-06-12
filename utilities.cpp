8747e13c5b3e0e960d8181638e950b769612fbf3
billjordanchess <53726859+billjordanchess@users.noreply.github.com>
2021-06-12 20:34:13 +0930

    Update utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -10,7 +10,7 @@ char fen_name[256];
 int LoadDiagram(char* file)
 {
 int x,n=0;
-char ts[200];
+char ts[256];
 
 diagram_file = fopen(file, "r");
 if (!diagram_file)
@@ -23,16 +23,87 @@ strcpy_s(fen_name,file);
 
 fgets(ts, 256, diagram_file);
 
-//[FEN "B:W18,24,27,28,K10,K15:B12,16,20,K22,K25,K29"]
+int s, p;
 
-for(x=0;x<121;x++)
+int c = 0;
+int c1 = 0;
+
+char sq[3] = "";
+
+while(ts[c] != ':')
 {
-	board[x]= ts[x] - 48;
+	c++;
 }
-if(ts[121]=='1')
-	side = 1;
+
+if(ts[c - 1]=='W')
+{
+	side = LIGHT;
+	xside = DARK;
+}
 else
-	side = 2;
+{
+	side = DARK;
+	xside = LIGHT;
+}
+if(ts[c + 1]=='W')
+	s = LIGHT;
+else
+	s = DARK;
+
+c++;
+memset(board,0,sizeof(board));
+
+while(ts[c++])
+{
+	while(ts[c] != ',' && ts[c] != '"' && ts[c] != ':')
+	{
+		if(ts[c] == 'K')
+		{
+			if(s==LIGHT)
+				p = RK;
+			else
+				p = BK;
+			c++;
+			continue;
+		}
+		if(ts[c] == 'B')
+		{
+	        s = DARK;
+			c += 1;
+			continue;
+		}
+		if(ts[c] == 'W')
+		{
+	        s = LIGHT;
+			c += 1;
+			continue;
+		}
+		if(c1<3)
+			sq[c1++] = ts[c];
+		c++;
+	}
+	if(c1<3)
+	  sq[c1] = 0;
+	x = atoi(sq) - 1;
+
+	if(p!=RK && p!=BK)
+	{
+		if(s==LIGHT)
+			p = RM;
+		else
+			p = BM;
+	}
+	if(x>=0 && x<32)
+	{
+	  board[x] = p;
+	  p = 0;
+	}
+	c1 = 0;
+	if(ts[c] == '"')
+	{
+		break;
+	}
+}
 
 CloseDiagram();
 DisplayBoard(0,0);
@@ -71,7 +142,7 @@ for(int x=0;x<32;x++)
 	{
 		if(wc>0)
 			strcat(b,",");
-		strcat(b,itoa(x,a,10));
+		strcat(b,itoa(x+1,a,10));
 		wc++;
 	}
 	if(board[x]==RK)
@@ -79,7 +150,7 @@ for(int x=0;x<32;x++)
 		if(wc>0)
 			strcat(b,",");
 		strcat(b,"K");
-		strcat(b,itoa(x,a,10));
+		strcat(b,itoa(x+1,a,10));
 		wc++;
 	}	
 }
@@ -90,7 +161,7 @@ for(int x=0;x<32;x++)
 	{
 		if(bc>0)
 			strcat(b,",");
-		strcat(b,itoa(x,a,10));
+		strcat(b,itoa(x+1,a,10));
 		bc++;
 	}
 	if(board[x]==BK) 
@@ -98,18 +169,16 @@ for(int x=0;x<32;x++)
 		if(bc>0)
 			strcat(b,",");
 		strcat(b,"K");
-		strcat(b,itoa(x,a,10));
+		strcat(b,itoa(x+1,a,10));
 		bc++;
 	}		
 }
 
 strcat(b,"\"]");
-//[FEN "B:W18,24,27,28,K10,K15:B12,16,20,K22,K25,K29"]
 
 fwrite(b,sizeof(char), sizeof(b),diagram_file);
 
 if (diagram_file)
     fclose(diagram_file);
 diagram_file = NULL;
-
 }
